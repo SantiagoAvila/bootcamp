66a5f72af1040b1669ead505ce6e066c4ae05e41
Santiago Avila <savila1810@gmail.com>
2019-04-24 16:25:58 -0400

    es una clase para definir temp y pres

---
+++ objetos.py
@@ -0,0 +1,18 @@
+class condicion_estandar:
+    temperaturastandar= 20 
+    presionstandar= 1
+    def __init__(self,temperatura,presion):
+        self.temperatura=temperatura
+        self.presion=presion
+        print("la temperatura actual es de",self.temperatura,"grados centigrados")
+        print("la presion actual es de ",self.presion,"atm")
+    def set_temperatura(self,cantidad):
+        self.temperatura = cantidad
+    def set_presion(self,cantidad):
+        self.presion=cantidad
+
+condicion_estandar(22,1.05)
+
+meteorologia_estandar=condicion_estandar(22,1.01)
+
+meteorologia_actual=condicion_estandar(25,1.03)
