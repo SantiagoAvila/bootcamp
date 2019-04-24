f88a948a75c599e2e71570a8e82a677f96c80398
Santiago Avila <savila1810@gmail.com>
2019-04-24 18:11:17 -0400

    objeto personas con funcion cumpleaños

--- persona.py
+++ persona.py
@@ -1,6 +1,22 @@
-print ("Hola Mundo")
-#esto es una prueba 
-pritn("que tal muuundooo")
-print("no chau")
-print("FGNFHJ")
-print("aca modifico")
+class personas:
+    def __init__(self,nombre,edad):
+        self.Nombre=nombre
+        self.edad=edad
+        print("Mi nombre es",self.Nombre,"y tengo",self.edad,"años")
+    def set_Nombre(self,cantidad):
+        self.Nombre = cantidad
+    def set_edad(self,cantidad):
+        self.edad=cantidad
+
+    def cumple_años(self):
+        i=1
+        a=2019
+        while i<11:
+            self.edad=self.edad+1
+            print(self.edad,"años en",a)
+            i=i+1
+            a=a+1
+
+pepito = personas("Santiago",25)
+
+
