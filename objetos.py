a2e23fcb3e410883fcc8dca6ddac235638f4d06f
Santiago Avila <savila1810@gmail.com>
2019-04-24 17:00:22 -0400

    con las mediciones estandar

--- objetos.py
+++ objetos.py
@@ -1,18 +1,21 @@
 class condicion_estandar:
-    temperatura= 20 
-    presion= 1
+    temperaturastandar= 20 
+    presionstandar= 1
     def __init__(self,temperatura,presion):
         self.temperatura=temperatura
         self.presion=presion
         print("la temperatura actual es de",self.temperatura,"grados centigrados")
-        print("la presion actual es de ",self.presion,"atm")
+        print("la presion actual es de ",self.presion,"atm, siendo la estandar")
     def set_temperatura(self,cantidad):
         self.temperatura = cantidad
     def set_presion(self,cantidad):
         self.presion=cantidad
 
-condicion_estandar(22,1.05)
+condicion_estandar(22,1)
 
 meteorologia_estandar=condicion_estandar(22,1.01)
 
 meteorologia_actual=condicion_estandar(25,1.03)
+
+condicion_estandar(27,1.01)
+
