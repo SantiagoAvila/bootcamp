7aa126df5c78b5fc7810d3598e3a1f88db8485b7
Santiago Avila <savila1810@gmail.com>
2019-04-25 13:55:54 -0400

    ejemlos de diccionarios

---
+++ prueba_dicc.py
@@ -0,0 +1,19 @@
+diccionario={}
+
+nombre_de_diccionario={"nombre_clave":"valor"}
+### virtudes de una lista es que no debes memorizar el  orden de los factores para rellenar o modificar
+
+#### ejemplo de diccionario
+dic_persona["edad"]
+
+####cambiar elemento de diccionario
+dic_persona["edad"]="adivina mi edad"
+print(dic_persona["edad"])
+
+dic_persona["profesion"]="programador"
+
+print(dic_persona)
+
+#### si no hay datos puedes utilizar get
+print(dic_persona.get("estatura"))
+
