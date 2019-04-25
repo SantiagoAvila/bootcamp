910a5c808f06c2ede474a7ca42ccf41f262be60f
Santiago Avila <savila1810@gmail.com>
2019-04-25 17:26:28 -0400

    a

--- req.py
+++ req.py
@@ -2,7 +2,7 @@
 import requests
 from pprint import pprint
 def datos_peli(Nombre_peli):
-    API_KEY= ""
+    API_KEY= "aqui va usuario"
     URL= "http://www.omdbapi.com/?apikey="
     titulo=Nombre_peli
     busqueda= URL + API_KEY + "&t=" + titulo
