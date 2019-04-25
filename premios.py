f08de97cf7d861ae031f94d6784b787aebf1a836
Santiago Avila <savila1810@gmail.com>
2019-04-25 11:07:10 -0400

    uno puede elegir el numero de sorteos y lista

--- premios.py
+++ premios.py
@@ -1,10 +1,16 @@
 from random import randint
 parti = ["pepito","juan","pablo","marcela","santiago"]
-sorteo=1
-while sorteo<4:
-    x=len(parti)-1
-    y=randint(0,x)
-    z= parti[y]
-    print("el ganador numero",sorteo,"es",z)
-    parti.pop(y)
-    sorteo=sorteo+1
+def sorteos(cantidad_premios,lista):
+    if cantidad_premios <= len(lista):
+        sorteo=1
+        while sorteo<cantidad_premios+1:
+            x=len(lista)-1
+            y=randint(0,x)
+            z= lista[y]
+            print("el ganador numero",sorteo,"es",z)
+            lista.pop(y)
+            sorteo=sorteo+1
+    else:
+        print("tu numero de premios supera al de participantes... dejate de joder")
+
+sorteos(2,parti)
